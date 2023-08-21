c53f8e0fa760e7882fa2d45bba0b07de37fe63ae
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-08-21 04:04:07 -0400

    original api setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import { useState, useEffect } from "react";
 import HelloWorld from "./components/HelloWorld";
 
 
 function App() {
+  //api fetch
+  const [food, setFood] = useState(null);
+  const [search, setSearch] = useState('');
+
+  const getFood = async () => {
+    const url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=pasta&instructionsRequired=true&fillIngredients=false&addRecipeInformation=false&ignorePantry=true&sort=calories&sortDirection=asc&minCholesterol=0&minFluoride=0&minSaturatedFat=0&minVitaminA=0&minVitaminC=0&minVitaminD=0&minVitaminE=0&minVitaminK=0&minVitaminB1=0&minVitaminB2=0&minVitaminB5=0&minVitaminB3=0&minVitaminB6=0&minVitaminB12=0&minFiber=0&minFolate=0&minFolicAcid=0&minIodine=0&minIron=0&minMagnesium=0&minManganese=0&minPhosphorus=0&minPotassium=0&minSelenium=0&minSodium=0&minSugar=0&minZinc=0&offset=0';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '694f8ca701mshb554bf60b529ddbp1bcd34jsn1b011d23fbef',
+        'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+     setFood(result.results)
+     console.log(result.results)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(()=> {
+    getFood()
+  },[])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //components and content
   return (
     <div className="App">
       <HelloWorld />
